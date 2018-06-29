11ee7a5def34f612ffed30805ccf30f4ddfc0a5b
Kunszt Norbert <kunszt.norbert@gmail.com>
2018-06-29 16:41:31 +0200

    route test for subdomain, but not work...

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/testwhere', 'TestController@testWhere');
 
 Route::get('/hash/{string}', function ($string) {
