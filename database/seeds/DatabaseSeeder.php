f0379f480d9b1614409b647315e415972730079e
Kunszt Norbert <kunszt.norbert@gmail.com>
2018-07-07 13:41:18 +0200

    fix some bug create artisan command for build database change user seeder seed permission form me remove some config file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call(UserSeeder::class);
         $this->call(HolidayTypesSeeder::class);
         $this->call(CompaniesSeeder::class);
+        $this->call(PermissionSeeder::class);
     }
 }
